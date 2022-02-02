67464bdf35d20cc6ca66f8180f674354cf84655e
Aileen <acg.bcc@gmail.com>
2022-02-02 11:52:55 -0800

    Fixed parse errors

--- force-app/main/default/lwc/searchBox/searchBox.js
+++ force-app/main/default/lwc/searchBox/searchBox.js
@@ -2,12 +2,13 @@ import { LightningElement, wire, api } from 'lwc';
 
 export default class SearchAccounts extends LightningElement {
     @api key;
-    // -- ONCHANGE INPUT HANDLER FUNCTION (gets data from html) ---
+    // -- ONCHANGE INPUT HANDLER FUNCTION (gets data from html: clicked or not) ---
     updateKey(event){
         this.key = event.target.value;
-
+    }
 
     // -- ONCLICK BUTTON HANDLER FUNCTION 
+    // -- Search begins when button is clicked
     handleSearch(){
         this.key = event.target.value;
         
