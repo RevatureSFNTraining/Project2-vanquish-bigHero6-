5f6e8f06bb4ff94fb2ffdf0311d6cdc79a1fdba7
leewash3 <95656577+leewash3@users.noreply.github.com>
2022-02-02 14:13:23 -0800

    Add files via upload

---
+++ force-app/main/default/lwc/inspirationStory/inspirationStory.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+
+export default class LightningExampleAccordionMultiple extends LightningElement {
+    activeSections = ['1', '4'];
+    activeSectionsMessage = '';
+
+    handleSectionToggle(event) {
+        const openSections = event.detail.openSections;
+
+        if (openSections.length === 0) {
+            this.activeSectionsMessage = 'All sections are closed';
+        } else {
+            this.activeSectionsMessage =
+                'Open sections: ' + openSections.join(', ');
+        }
+    }
+}
